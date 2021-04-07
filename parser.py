a4895cf1bddcb89b3cc3279602b706c056af79ac
Mafer Lopez <lop17160@uvg.edu.gt>
2021-04-07 13:28:31 -0600

    scanner para extraer las caracteristicas que conforman al lenguaje

---
+++ parser.py
@@ -0,0 +1,12 @@
+from automatas import tree
+from automatas import direct
+from automatas import thompson
+from automatas import utils
+
+#documentacion from page 6 of pdf https://ssw.jku.at/Research/Projects/Coco/Doc/UserManual.pdf 
+
+
+RESERVERD_KEYWORDS = ["ANY", "CHARACTERS", "COMMENTS", "COMPILER", "CONTEXT",
+"END", "FROM", "IF", "IGNORE", "IGNORECASE", "NESTED", "out", "PRAGMAS",
+"PRODUCTIONS", "SYNC", "TO", "TOKENS", "WEAK"]
+
---
+++ scanner.py
@@ -0,0 +1,148 @@
+
+
+COMMENTS = ["/*", "*/", "//"]
+
+def read_word(file, actual):
+    word = ""
+    while actual < len(file):
+        if (file[actual] == " " or file[actual] == "\n") and (len(word) > 0):
+            break
+        elif file[actual] == " " or file[actual] == "\n":
+            actual += 1
+        else:
+            word += file[actual]
+            actual += 1
+    return word, actual
+
+def scanner(file):
+    actual = 0
+    characters = []
+    keywords = []
+    tokens = []
+    temp_word = ""
+    while True:
+        temp_word, actual = read_word(file, actual)
+        if temp_word == "COMPILER":
+            name, actual = get_compiler(file, actual)
+        if temp_word == "CHARACTERS":
+            characters, actual = get_chars(file, actual)
+        if temp_word == "KEYWORDS":
+            keywords, actual = get_keywords(file, actual)
+        if temp_word == "TOKENS":
+            tokens, actual = get_tokens(file, actual)
+        if temp_word == "PRODUCTIONS":
+            get_productions(file, actual)
+        if temp_word == "END":
+            final = get_end(file, actual, name)
+            if final:
+                break
+            else:
+                print("No coincide el nombre")
+                break
+    
+    print(name)
+    print(characters)
+    print(keywords)
+    print(tokens)
+    return name, characters, keywords, tokens
+
+def get_compiler(file, actual):
+    actual += 1 
+    name, actual = read_word(file, actual)
+    return name, actual
+
+def get_chars(file, actual):
+    print("leyendo CHARACTERS")
+    actual += 1
+    temp = ""
+    characters = {}
+    temp_id = ""
+    temp_values = ""
+    line = ""
+    while True:
+        temp, actual = read_word(file, actual) 
+        if temp == "KEYWORDS":
+            actual -= 8
+            break
+        line += temp
+        if line[-1] == "." and line[-2] != ".":
+            if "=" in line:
+                completo = line.split("=")
+                temp_id = completo[0]
+                temp_values = completo[1]
+                #print("id: ",temp_id, " values: ", temp_values)
+                characters[temp_id] = temp_values
+                line =  ""
+            else:
+                print("no se encuentra el '='")
+    return characters, actual
+
+def get_keywords(file, actual):
+    print("leyendo KEYWORDS")
+    actual += 1
+    temp = ""
+    keywords = {}
+    temp_id = ""
+    temp_values = ""
+    line = ""
+    while True:
+        temp, actual = read_word(file, actual) 
+        if temp == "TOKENS":
+            actual -= 6
+            break
+        line += temp
+        if line[-1] == ".":
+            if "=" in line:
+                completo = line.split("=")
+                temp_id = completo[0]
+                temp_values = completo[1]
+                keywords[temp_id] = temp_values
+                line =  ""
+            else:
+                print("no se encuentra el '='")
+    return keywords, actual
+
+def get_tokens(file, actual):
+    print("leyendo TOKENS")
+    actual += 1
+    temp = ""
+    tokens = {}
+    temp_id = ""
+    temp_values = ""
+    line = ""
+    while True:
+        temp, actual = read_word(file, actual) 
+        if temp == "PRODUCTIONS":
+            actual -= 11
+            break
+        if temp == "END":
+            actual -= 3
+            break
+        line += temp
+        if line[-1] == ".":
+            if "=" in line:
+                completo = line.split("=")
+                temp_id = completo[0]
+                temp_values = completo[1]
+                tokens[temp_id] = temp_values
+                line =  ""
+            else:
+                print("no se encuentra el '='")
+    return tokens, actual
+
+def get_productions(file, actual):
+    return actual
+
+def get_end(file, actual, name):
+    actual += 1
+    end_name, actual = read_word(file, actual)
+    if end_name == name:
+        return True
+    return False
+
+
+if __name__ == "__main__":
+    file = open("input/Ejemplo.txt")
+    content = file.read()
+    scanner(content)
+    file.close()
